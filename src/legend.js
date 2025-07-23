a84c7fcdc7ce0a391a080c865d0bd0ef3c1fdb16
Eugene <evgenij.biryukov.99@mail.ru>
2025-07-23 21:46:49 +0300

    refactor: change style classes into constants

--- src/legend.js
+++ src/legend.js
@@ -1,16 +1,23 @@
+const LEGEND_CLASS = 'legend';
+const LEGEND_TITLE_CLASS = 'legend-title';
+const LEGEND_ITEM_CLASS = 'legend-item';
+const LEGEND_COLOR_CLASS = 'legend-color';
+const LEGEND_PLAN_CLASS = 'legend-plan';
+const LEGEND_ACTUAL_CLASS = 'legend-actual';
+
 export function createLegend() {
   const legend = document.createElement('div');
-  legend.className = 'legend';
+  legend.className = LEGEND_CLASS;
 
   const title = document.createElement('div');
-  title.className = 'legend-title';
+  title.className = LEGEND_TITLE_CLASS;
   title.textContent = 'Обозначения';
 
   const planItem = document.createElement('div');
-  planItem.className = 'legend-item';
+  planItem.className = LEGEND_ITEM_CLASS;
   
   const planColor = document.createElement('div');
-  planColor.className = 'legend-color legend-plan';
+  planColor.classList.add(LEGEND_COLOR_CLASS, LEGEND_PLAN_CLASS);
   
   const planText = document.createElement('div');
   planText.textContent = 'Плановая скважина';
@@ -19,10 +26,10 @@ export function createLegend() {
   planItem.append(planText);
 
   const actualItem = document.createElement('div');
-  actualItem.className = 'legend-item';
+  actualItem.className = LEGEND_ITEM_CLASS;
   
   const actualColor = document.createElement('div');
-  actualColor.className = 'legend-color legend-actual';
+  actualColor.classList.add(LEGEND_COLOR_CLASS, LEGEND_ACTUAL_CLASS);
   
   const actualText = document.createElement('div');
   actualText.textContent = 'Фактическая скважина';
